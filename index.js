25e5110298b50353566ed76abcab02db511c2d0c
Zion Miller <djzionmiller@gmail.com>
2022-06-29 14:28:09 -0700

    commit tree c005b80401afb80e74c665f814e4195d3c877e2e

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+    const cool = document.getElementById('text');
+    cool.innerHTML = ("This is really cool!");
+});
